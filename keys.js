3c9c374bf5e746f50d74caa03396c924f79745fa
Brian Jin <brnjin6@gmail.com>
2017-10-30 17:54:42 -0400

    Starter files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '4TB6h3ypJsGqj61Dly6EF4aR1',
+  consumer_secret: 'n6x2I0TMI8lID5qPntUwHyZWmTTat8mU4AqJZLRquBWVDqvOYB',
+  access_token_key: '922561725228027905-6YUIxEofBMp7jcRE8dQLJbPwJVY72uh',
+  access_token_secret: 'tSxXxJ9JjUquCvajU0P2fFV6iAHpZB9alJqnnKk2D8FI5',
+}
+
+module.exports = twitterKeys;
